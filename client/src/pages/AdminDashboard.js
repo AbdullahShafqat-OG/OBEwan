7b3bb209df1df40f2475ce7c4be959a94b1a2fa4
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2022-04-20 13:54:05 +0500

    Changed UI of admin dashboard

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -1,119 +1,190 @@
 import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Button, IconButton, Typography } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+    secondary: {
+      main: "#489BFC",
+    },
+    text: { primary: "#000000", secondary: "#489BFC" },
+  },
+  typography: {
+    fontFamily: ['"DM Sans"', "sans-serif"].join(","),
+    button: {
+      fontWeight: "700",
+      fontSize: "16px",
+      textTransform: "none",
+    },
+    h4: {
+      fontWeight: "700",
+      fontSize: "32px",
+    },
+    h5: {
+      fontWeight: "700",
+      fontSize: "24px",
+    },
+    h6: {
+      fontWeight: "700",
+      fontSize: "20px",
+    },
+    subtitle: {
+      fontFamily: "DM Sans",
+      fontWeight: "500",
+    },
+  },
+});
 
 const Plo = (props) => (
-    <tr>
-        <td>{props.plo.name}</td>
-        <td>{props.plo.label}</td>
-        <td>{props.plo.statement}</td>
-        <td>{props.plo.degree}</td>
-    </tr>
+  <TableRow>
+    <TableCell>{props.plo.name}</TableCell>
+    <TableCell>{props.plo.label}</TableCell>
+    <TableCell>{props.plo.statement}</TableCell>
+    <TableCell>{props.plo.degree}</TableCell>
+    <TableCell>
+      <Stack direction="row" spacing={0}>
+        <IconButton>
+          <EditIcon></EditIcon>
+        </IconButton>
+        <IconButton>
+          <DeleteIcon></DeleteIcon>
+        </IconButton>
+      </Stack>
+    </TableCell>
+  </TableRow>
 );
 
 const Course = (props) => (
-    <tr>
-        <td>{props.course.name}</td>
-        <td>{props.course.instructors}</td>
-        <td>button or cascading view for instructors</td>
-    </tr>
+  <TableRow>
+    <TableCell>{props.course.name}</TableCell>
+    <TableCell>{props.course.instructors}</TableCell>
+    <TableCell>button or cascading view for instructors</TableCell>
+  </TableRow>
 );
 
 export default function PloList() {
-    const [plos, setPlos] = useState([]);
-    const [courses, setCourses] = useState([]);
+  const [plos, setPlos] = useState([]);
+  const [courses, setCourses] = useState([]);
 
-    useEffect(() => {
-        async function getPlos() {
-            const response = await fetch('http://localhost:3000/api/plos/');
+  useEffect(() => {
+    async function getPlos() {
+      const response = await fetch("http://localhost:3000/api/plos/");
 
-            if (!response.ok) {
-                const message = `An error occurred: ${response.statusText}`;
-                window.alert(message);
-                return;
-            }
-
-            const plos = await response.json();
-            setPlos(plos);
-        }
-
-        getPlos();
-
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
         return;
-    }, [plos.length]);
+      }
 
-    function ploList() {
-        return plos.map((plo) => {
-            return (
-                <Plo 
-                    plo={plo} 
-                    key={plo._id}
-                />
-            );
-        });
+      const plos = await response.json();
+      setPlos(plos);
     }
 
-    useEffect(() => {
-        async function getCourses() {
-            const response = await fetch('http://localhost:3000/api/courses/');
+    getPlos();
 
-            if (!response.ok) {
-                const message = `An error occurred: ${response.statusText}`;
-                window.alert(message);
-                return;
-            }
+    return;
+  }, [plos.length]);
 
-            const courses = await response.json();
-            setCourses(courses);
-        }
+  function ploList() {
+    return plos.map((plo) => {
+      return <Plo plo={plo} key={plo._id} />;
+    });
+  }
 
-        getCourses();
+  useEffect(() => {
+    async function getCourses() {
+      const response = await fetch("http://localhost:3000/api/courses/");
 
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
         return;
-    }, [courses.length]);
+      }
 
-    function courseList() {
-        return courses.map((course) => {
-            return (
-                <Course 
-                    course={course} 
-                    key={course._id}
-                />
-            );
-        });
+      const courses = await response.json();
+      setCourses(courses);
     }
 
-    function testFunction() {
-        window.location.href = "/dashboard/admin/createPlo";
-    }
+    getCourses();
 
-    return (
-        <div>
-            <h1>Admin Dashboard</h1>
-            <h3>PLO List</h3>
-            <table border="1px">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Label</th>
-                        <th>Statement</th>
-                        <th>Degree</th>
-                    </tr>
-                </thead>
-                <tbody>{ploList()}</tbody>
-            </table>
-            <h3>Courses</h3>
-            <table border="1px">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Instructors</th>
-                        <th>Mapping</th>
-                    </tr>
-                </thead>
-                <tbody>{courseList()}</tbody>
-            </table>
-            <br />
-            <button onClick={() => {testFunction()}}>Create PLO</button>
-        </div>
-    );
+    return;
+  }, [courses.length]);
+
+  function courseList() {
+    return courses.map((course) => {
+      return <Course course={course} key={course._id} />;
+    });
+  }
+
+  function testFunction() {
+    window.location.href = "/dashboard/admin/createPlo";
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Typography variant="h4" align="center" sx={{ m: "20px" }}>
+          Admin Dashboard
+        </Typography>
+        <Typography variant="h5">Courses</Typography>
+        <Table sx={{ border: "1px" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Instructors</TableCell>
+              <TableCell>Mapping</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{courseList()}</TableBody>
+        </Table>
+        <br />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography variant="h5">PLO List</Typography>
+          <Button
+            startIcon={<AddIcon></AddIcon>}
+            variant="contained"
+            align="right"
+            onClick={() => {
+              testFunction();
+            }}
+          >
+            Create PLO
+          </Button>
+        </Box>
+        <Table sx={{ border: "1px" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Label</TableCell>
+              <TableCell>Statement</TableCell>
+              <TableCell>Degree</TableCell>
+              <TableCell align="center">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{ploList()}</TableBody>
+        </Table>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
